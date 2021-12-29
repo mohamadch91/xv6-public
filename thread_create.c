3e20a0d4540af28bebdd7fa4eed7c5aa624f71e3
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-12-29 00:02:48 -0800

    create thread create fiel
    implementation of function to create thread

---
+++ thread_create.c
@@ -0,0 +1,38 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#define PAGESIZE 4096
+
+//thread create function
+int thread_create(void (*fn) (void*),void* arg){
+    void *fptr = malloc(2 * (PAGESIZE));
+    void *stack;
+    if(fptr == 0){
+        fprintf("thread_create: out of memory\n");
+        return -1;
+
+    }
+    int mod=(uint)fptr%PAGESIZE;
+    if(mod!=0){
+        stack=fptr+PAGESIZE-mod;
+    }
+    else{
+        stack=fptr;
+    }
+    int thread_id=clone((void*)stack);
+    //check the CLONE
+    if(thread_id<0){
+        fprintf(1,"thread_create: clone failed\n");
+        // return -1;
+    }
+    //child
+    if(thread_id==0){
+        //call the function
+        fn(arg);
+        //fre the stack
+        free(stack);
+        //exit
+        exit();
+    }
+    return thread_id;
+}
